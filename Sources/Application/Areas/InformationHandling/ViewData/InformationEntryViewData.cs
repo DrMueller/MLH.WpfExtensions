68f6b2a1f231d5fbecb1e4055a0a9f8d077460a1
Matthias Mueller <matthiasm@live.de>
2018-08-13 11:22:14 +0200

    WIP

--- Sources/Application/Areas/InformationHandling/ViewData/InformationEntryViewData.cs
+++ Sources/Application/Areas/InformationHandling/ViewData/InformationEntryViewData.cs
@@ -8,7 +8,7 @@
             ShowBusyIndicator = showBusyIndicator;
         }
 
-        public string BusyIndicatorSource => "/Mmu.Mlh.WpfExtensions;component/Infrastructure/Assets/FA_Cog_Green.png";
+        public static string BusyIndicatorSource => "/Mmu.Mlh.WpfExtensions;component/Infrastructure/Assets/FA_Cog_Green.png";
         public string Message { get; }
         public bool ShowBusyIndicator { get; }
     }
